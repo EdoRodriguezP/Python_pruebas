16cab001e3c48defa5879fe13c352899c07223a1
EdoRodriguezP <ponce1604@gmail.com>
2025-05-08 19:37:26 -0400

    primeros acercamientos a python

---
+++ ejercicio.py
@@ -0,0 +1,30 @@
+
+"""
+resultado = "gato" * 3
+
+print(resultado)
+
+name = "juancho"
+apellido = "rosselberg"
+edad = 18
+
+print(len(name))
+
+x = 'awesome'
+def myfunc():
+    x = 'fantastic'
+    print('Python is ' + x)
+myfunc()
+print('Python is ' + x)
+"""
+x = 5
+
+print(type(x))
+
+txt = "The best things in life are free!"
+print("expensive" not in txt)
+x = 'Welcome'
+print(x[3:5])
+
+a = "Hello, World!"
+print(a.lower())
