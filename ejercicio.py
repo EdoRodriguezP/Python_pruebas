8a98d7de6aee909d2f0baa62de739d72a83224e4
EdoRodriguezP <ponce1604@gmail.com>
2025-05-08 21:14:45 -0400

    se continua en ejercicios

--- ejercicio.py
+++ ejercicio.py
@@ -16,7 +16,7 @@ def myfunc():
     print('Python is ' + x)
 myfunc()
 print('Python is ' + x)
-"""
+
 x = 5
 
 print(type(x))
@@ -28,3 +28,31 @@ print(x[3:5])
 
 a = "Hello, World!"
 print(a.lower())
+
+
+peso = float(input("cual es tu peso ? :"))
+altura = float(input("cual es tu altura ? : "))
+
+imc = peso / (altura ** 2)
+print(f"el IMC es : {imc:.3f}")
+
+
+precio_original = int(input("Cual es el precio? :"))
+descuento = float(input("cual es el porcentaje de descuento? :"))
+
+precio_descuento = precio_original * (descuento / 100)
+precio_final = precio_original - precio_descuento
+
+print(f"precio con descuento es : {precio_final}")
+
+
+nota1 = float(input("nota 1 :"))
+nota2 = float(input("nota 2 :"))
+nota3  = float(input("nota 3 :"))
+
+nota_final = (nota1 + nota2 + nota3) / 3
+
+print(f"nota final : {nota_final:.1f}")
+"""
+
+
