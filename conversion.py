aa455a0fe70045cf8b9bc119a31634525d08caf2
EdoRodriguezP <ponce1604@gmail.com>
2025-05-28 21:57:53 -0400

    s

--- conversion.py
+++ conversion.py
@@ -1,12 +1,12 @@
 import sys
 
-def validar_argumentos():     # Funcion para validar entrada de 4 argumentos
-    if len(sys.argv) != 2:    # Si los argumentos son diferente a 4 imprime 
-        print("Error: Se debe ingresar 4 argumentos")
+def validar_argumentos():     # Funcion para validar entrada de argumentos
+    if len(sys.argv) != 2:    # Si los argumentos son diferente a 2 imprime error y uso
+        print("Error: Se debe ingresar 2 argumentos")
         print("Uso: python conversiones.py [archivo]")
         sys.exit(1)
 
-archivo = sys.argv[1]
+archivo = sys.argv[1]             # Se crea variable archivo y se le asigna el archivo de texto
 
 with open(archivo, "r") as f:
     texto = f.read()
